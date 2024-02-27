72db2d99b5dacfce2f6e7993398398cd0d0673ba
Jack Le Hamster <jacklehamster@gmail.com>
2024-02-27 12:32:57 -0800

    Auto-commit: 2024-02-27 12:32:57
    Files:
    src/matrix/IPositionMatrix.ts
    
    https://github.com/jacklehamster/autocommit

--- src/matrix/IPositionMatrix.ts
+++ src/matrix/IPositionMatrix.ts
@@ -13,6 +13,7 @@ export interface IPositionMatrix extends IMatrix {
   moveBy(x: number, y: number, z: number, turnMatrix?: IMatrix): MoveResult;
   moveTo(x: number, y: number, z: number): MoveResult;
   moveTowards(x: number, y: number, z: number, speed?: number): MoveResult;
+  attemptMoveTowards(x: number, y: number, z: number, speed?: number): MoveResult;
   movedTo(x: number, y: number, z: number): this;
   get position(): Vector;
   blockers?: List<ICollisionDetector>;
